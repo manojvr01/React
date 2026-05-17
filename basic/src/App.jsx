004f6134c55f870817a645d0f7a7ce1ba57245c4
manojvr01 <manojvrgowda2004@gmail.com>
2026-05-17 17:00:02 +0530

    basic things

---
+++ basic/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>fir se fat gayi na </h1>
+      <h2>itna irritate mat hoja</h2>
+      <h3>thoda sa socha ,be calm bro</h3>
+    </div>
+  )
+}
+
+export default App
