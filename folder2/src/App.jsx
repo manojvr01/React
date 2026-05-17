569f55cf395e75b0124ecb3e3836bfc897c88a73
manojvr01 <manojvrgowda2004@gmaol.com>
2026-05-17 19:47:05 +0530

    components

---
+++ folder2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Name from './components/names'
+import Navbar from './components/navbar'
+
+const App = () => { 
+  return (
+    <div>
+      <Navbar />
+      <Name />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ folder2/src/components/names.jsx
@@ -0,0 +1,5 @@
+function name(){
+    return <div className="Card"><h1>John Doe</h1>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam libero error provident. Nihil aliquid sint at? Vero impedit a tenetur?</div>
+}
+export default name
